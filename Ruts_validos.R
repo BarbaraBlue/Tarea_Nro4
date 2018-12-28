980f6d246d154f541d0930e3107d80a7ea24d921
BarbaraBlue <barbaravaldesnunez@gmail.com>
2018-12-28 18:16:54 -0300

    Arreglo en tiempo que se demora en ejecutar la lista Ruts_validos.

--- Ruts_validos.R
+++ Ruts_validos.R
@@ -1,25 +1,21 @@
 source('Funciones.R')
 
+#t: tiempo que se demora en ejecutar un codigo
+#def t: tiempo que se demora en recorrer la lista Ruts_validos
 #Ruts_validos: Generar numeros de rut y almacenarlos en una lista
 #Se generan 5000 numeros aleatorios entre 0:9999999 sin repeticion 
-#Cada uno acompañado de su dv 
+#Cada uno acompañado de su dv
 
+t <- proc.time()
 Ruts_validos <- list()
 for(i in sample(0000000:9999999,5000,replace=F)){
   rut<- print(paste(i,"-",dv(i)))
   Ruts_validos<-c(Ruts_validos,rut)
 }
-
-#t: tiempo que se demora en ejecutar un codigo
-#def t: tiempo que se demora en recorrer la lista Ruts_validos
-
-t <- proc.time()
-for(rut in Ruts_validos){
-   print(rut)
-}
 proc.time() - t
 
-*/user  system elapsed 
-5.11    0.06    5.17   /*
-#R: El tiempo de ejecucion del cod es 5.17 segs
-  
+#Tiempo que se demora en recorrer la lista Ruts_validos
+> proc.time() - t
+user  system elapsed 
+5.34    0.08    5.50 
+>   
