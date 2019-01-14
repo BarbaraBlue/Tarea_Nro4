12a30314c375c3063c671af2cdb573bdd7e1975f
BarbaraBlue <barbaravaldesnunez@gmail.com>
2019-01-14 02:12:26 -0300

    Se agrega al archivo Funciones.R la función strsplit para contar palabras

--- Funciones.R
+++ Funciones.R
@@ -1,6 +1,7 @@
 #Establecer carpeta 'Tarea_Nro4" como directorio de trabajo
 setwd("~/Tarea_Nro4")
 
+#FUNCION A UTILIZAR PARA OBTENER EL DV DE CADA RUT
 #dv: int -> int
 #Def dv: funcion para generar digito verificador para un rut valido
 #Ejemplo: dv(18749743)->4
@@ -16,3 +17,12 @@ dv <- function(rut){
   dv = val[match(z, key)]
   return(dv)
 }
+
+
+#FUNCION A UTILIZAR PARA CONTAR PALABRAS
+#strsplit: string(a,b) -> número de palabras
+#def strsplit: función que cuenta palabras, donde a es el contenido y b un separador
+
+Contando_palabras <- strsplit(frase," ")[1]
+strsplit(" Cuando plancha Pancha, siempre plancha con más de una plancha, ¿con cuántas planchas plancha Pancha?"," ")[[1]]
+
